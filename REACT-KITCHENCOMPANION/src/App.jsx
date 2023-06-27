37adbff5a9a7679ce93f9adf76a6a002afd32bb2
Your Name <you@example.com>
2023-06-27 14:34:09 -0400

    react install

---
+++ REACT-KITCHENCOMPANION/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './component/navbar.jsx'
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar/>
+    
+      
+      
+    </>
+  )
+}
+
+export default App
