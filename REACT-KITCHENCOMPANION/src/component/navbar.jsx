f87c524296845403aade5639a8d44f996a5f2369
Your Name <you@example.com>
2023-06-28 17:03:18 -0400

    pach

--- REACT-KITCHENCOMPANION/src/component/navbar.jsx
+++ REACT-KITCHENCOMPANION/src/component/navbar.jsx
@@ -1,45 +1,75 @@
+import PropTypes from 'prop-types';
 
 
-const Navbar = () => {
-    return (
-      <nav className="navbar navbar-expand-lg bg-dark border-bottom border-bottom-dark" data-bs-theme="dark">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><hr className="dropdown-divider"/></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
+
+const Navbar = ({ empresa, home, nosotros, contacto, login, enmirefri }) => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-white   ">
+        <div className="container ">
+          <a className="navbar-brand text-black " href="/">
+            {empresa}
+          </a>
+          <button
+            className="navbar-toggler bg-white  "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav "
+            aria-expanded="false"
+            aria-label="Toggle navigation "
+          >
+            <span className="navbar-toggler-icon "></span>
+          </button>
+          <div
+            className="collapse navbar-collapse text-black  justify-content-end"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav  ">
+              <li className="nav-item  ">
+                <a
+                  className="nav-link active text-black  "
+                  aria-current="page"
+                  href="/"
+                >
+                  {home}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-black" href="/Nosotros">
+                  {nosotros}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-black" href="/Contactos">
+                  {contacto}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-black " href="/Enmirefri">{enmirefri}</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-black " href="/login">{login}</a>
+              </li>
+             
             </ul>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled">Disabled</a>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  </nav>
-      
-    )
-  }
-  
+  );
+};
+
+
+Navbar.propTypes = {
+  empresa: PropTypes.string.isRequired,
+  home: PropTypes.string.isRequired,
+  nosotros: PropTypes.string.isRequired,
+  contacto: PropTypes.string.isRequired,
+  login: PropTypes.string.isRequired,
+  enmirefri: PropTypes.string.isRequired,
+};
+
+
+
   export {Navbar}
