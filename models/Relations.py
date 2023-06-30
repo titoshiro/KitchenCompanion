c50f84b7ef2a8effb433aaae25fe0394b258a33f
Martin Donoso <mdonoso04@gmail.com>
2023-06-30 14:54:27 -0400

    Fixed relationship tables

--- models/Relations.py
+++ models/Relations.py
@@ -1,6 +1,11 @@
 from models import db
 
-class Ingredient_recipe(db.Model):
-    __tablename__='ingredient_recipe'
-    id_ingredient = db.Column(db.Integer, primary_key=True, db.ForeignKey('ingredients.id'), nullable=False)
-    id_recipe = db.Column(db.Integer, primary_key=True, db.ForeignKey('recipes.id'), nullable=False)
+ingredient_recipe = db.Table('ingredient_recipe',
+    db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredients.id'), ondelete="CASCADE", primary_key=True),
+    db.Column('recipe_id', db.Integer, db.ForeignKey('recipes.id'), ondelete="CASCADE", primary_key=True)
+)
+
+ingredient_user = db.Table('ingredient_user',
+    db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredients.id'), ondelete="CASCADE", primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), ondelete="CASCADE", primary_key=True)
+)
