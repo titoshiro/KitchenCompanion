44068fe47ee04b8aedc7b6eb004b83854d4094da
Your Name <you@example.com>
2023-07-04 15:17:08 -0400

    register

--- REACT-KITCHENCOMPANION/src/App.jsx
+++ REACT-KITCHENCOMPANION/src/App.jsx
@@ -1,33 +1,20 @@
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Nosotros from './pages/Nosotros'
-import Dashboard from './pages/admin/Dashboard'
-import Users from './pages/admin/users'
-import PrivateRoute from './utils/PrivateRoute'
-import Login from './pages/Login'
-import { ToastContainer } from 'react-toastify'
-import Contactos from './pages/Contactos'
-import Enmirefri from './pages/Enmirefri'
-=======
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppProvider } from './store/AppContext';
+import { ToastContainer } from 'react-toastify';
 import Home from './pages/Home';
 import Nosotros from './pages/Nosotros';
 import Dashboard from './pages/admin/Dashboard';
 import Users from './pages/admin/users';
 import PrivateRoute from './utils/PrivateRoute';
 import Login from './pages/Login';
-import { ToastContainer } from 'react-toastify';
 import Contactos from './pages/Contactos';
 import Enmirefri from './pages/Enmirefri';
-import { AppProvider } from './store/AppContext';
 import Register from './pages/Register';
->>>>>>> 730c03bf60524c97dc83dafebd1e765d026c1b97
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppProvider> {/* Agregar el AppProvider */}
+      <AppProvider>
         <Routes>
           <Route path="dashboard" element={<PrivateRoute />}>
             <Route index element={<Dashboard />} />
@@ -36,12 +23,11 @@ const App = () => {
           <Route path="/">
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
-            <Route path="Nosotros" element={<Nosotros />} />
-            <Route path="Contactos" element={<Contactos />} />
-            <Route path="Enmirefri" element={<Enmirefri />} />
+            <Route path="nosotros" element={<Nosotros />} />
+            <Route path="contactos" element={<Contactos />} />
+            <Route path="enmirefri" element={<Enmirefri />} />
             <Route index element={<Home />} />
           </Route>
-          <Route path="/Contactos" element={<PrivateRoute><Contactos /></PrivateRoute>} />
         </Routes>
       </AppProvider>
       <ToastContainer />
