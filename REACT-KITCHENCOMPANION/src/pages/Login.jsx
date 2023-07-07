84ce1bc84545ba0fd9c5835f046001ef3c9dbb3b
Martin Donoso <mdonoso04@gmail.com>
2023-07-06 23:10:17 -0400

    fixed login and register

--- REACT-KITCHENCOMPANION/src/pages/Login.jsx
+++ REACT-KITCHENCOMPANION/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Navbar } from '../component/navbar';
 
 const Login = () => {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = (e) => {
@@ -16,7 +16,7 @@ const Login = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name, password }),
+      body: JSON.stringify({ email, password }),
     })
       .then((response) => {
         if (response.ok) {
@@ -49,7 +49,7 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-          <input value={name} onChange={(e) => setName(e.target.value)} type="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
           <div id="emailHelp" className="form-text"></div>
         </div>
         <div className="mb-3">
